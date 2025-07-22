832f48bcd6013e378a5d9d96ed22ecb23e24bb25
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2025-07-22 01:33:44 -0400

    Adjusted the order of secret letters in the Lab 1 scavenger hunt so that they now align with the instructions and correctly spell out the intended secret word.

--- app/src/main/java/com/driuft/androidhunt/MainActivity.kt
+++ app/src/main/java/com/driuft/androidhunt/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- * [T]
+ * [J]
  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
